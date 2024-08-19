26c3ff1c45e4578ee88a23006980891e2a40b415
nareshnaresh47 <96810093+nareshnaresh47@users.noreply.github.com>
2024-08-19 21:00:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
  resource "aws_instance" "example" {
    ami           = "ami-0ae8f15ae66fe8cda"
    instance_type = "t2.micro"
-   key_name = "nginx"
+   key_name = "sonarkey"
    count="2"
 
 
